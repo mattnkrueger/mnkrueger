cb60cf4603c0c30c6f3c097ae04ecb18ab4ceb02
mattnkrueger <mkrue138@gmail.com>
2025-03-29 18:23:24 -0500

    adding university page

--- src/pages/UniversityPage/University.jsx
+++ src/pages/UniversityPage/University.jsx
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import '../../assets/css/Markdown.css';
+import universityEmblem from '../../assets/img/university_emblem.png';
 
 function UniversityPage() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    fetch('/src/pages/UniversityPage/university.md')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch markdown: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(text => {
+        setContent(text);
+      })
+      .catch(error => {
+        console.error('Error loading markdown:', error);
+        setContent('# Content could not be loaded\nPlease check the console for errors.');
+      });
+  }, []);
+
   return (
-    <div>
-      <ReactMarkdown></ReactMarkdown>
+    <div className="markdown-content">
+      {/* University header */}
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottom: '1px solid var(--color-border)',
+        paddingBottom: '0.3em',
+        marginBottom: '20px'
+      }}>
+        <h1 style={{ margin: 0, border: 'none', paddingBottom: 0 }}>University</h1>
+      </div>
+      
+      {/* University emblem section */}
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '40px 0'
+      }}>
+        <div style={{
+          position: 'relative',
+          width: '320px',
+          height: '320px',
+          borderRadius: '50%',
+          boxShadow: '0 8px 24px rgba(0, 0, 0, 0.25)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '0.1px'
+        }}>
+          <div style={{
+            width: '300px',
+            height: '300px',
+            borderRadius: '50%',
+            overflow: 'hidden',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white'
+          }}>
+            <img 
+              src={universityEmblem} 
+              alt="University of Iowa" 
+              style={{ 
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain'
+              }} 
+            />
+          </div>
+        </div>
+      </div>
+      
+      {/* Markdown content */}
+      <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
---
+++ src/pages/UniversityPage/university.md
@@ -0,0 +1,61 @@
+# University Classes
+Graduation Date: May 2026
+
+## Freshman Year
+Semester 1:
+- CHEM:1110 Principles of Chemistry I
+- ENGR:1000 Engineering Success
+- ENGR:1100 Intro to Engineering Problem Solving
+- ENGR:1300 Introduction to Engineering Computing
+- MATH:1560 Engineering Mathematics II: Multivariable Calculus
+
+Winter:
+- MATH:2550 Engineering Mathematics III: Matrix Algebra
+
+Semester 2:
+- ARTS:1060 Elements of Digital Photography
+- [CS:1210 Computer Science I: Fundamentals](https://github.com/mattnkrueger/CS-1210_Computer_Science_1)
+- MATH:2560 Engineering Mathematics IV: Differential Equations
+- PHYS:1611 Introductory Physics I
+
+## Sophomore Year
+Semester 1:
+- CS:2230 Computer Science II: Data Structures
+- ENGR:2120 Electrical Circuits
+- [ENGR:2730 Computers in Engineering](https://github.com/mattnkrueger/ENGR-2730_Computers_in_Engineering)
+- PHYS:2230 Introductory Physics II
+
+Semester 2:
+- CS:2210 Discrete Structures
+- ECE:2400 Linear Systems I
+- ECE:2410 Principles of Electronic Instrumentation
+- STAT:2020 Probability and Statistics for Engineering and Physical Science
+
+## Junior Year
+Semester 1:
+- ECE:3000 ECE Professional Seminar
+- ECE:3320 Introduction to Digital Design
+- [ECE:3330 Introduction to Software Design](https://github.com/mattnkrueger/ECE-3330_Introduction_to_Software_Design)
+- [ECE:3540 Communication Networks](https://github.com/mattnkrueger/ECE-3540_Communication_Networks)
+
+Semester 2:
+- CS:3330 Algorithms
+- CS:3820 Programming Language Concepts
+- [ECE:3350 Computer Architecture and Organization](https://github.com/mattnkrueger/ECE-3350_Computer_Architecture_and_Organization)
+- [ECE:3360 Embedded Systems](https://github.com/mattnkrueger/ECE-3360_Embedded_Systems)
+
+## Senior Year
+Semester 1:
+- TBD
+- TBD
+- TBD
+- TBD
+
+Semester 2:
+- TBD
+- TBD
+- TBD
+- TBD
+
+## Masters
+I plan on completing a Master's Degree to deepen my understanding of computer architecture
