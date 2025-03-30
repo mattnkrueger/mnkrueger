6bccc2c98c5e020d181a8463723a280d5cc8f060
mattnkrueger <mkrue138@gmail.com>
2025-03-29 21:59:32 -0500

    cant figure this out. will have to watch yt... i am doing everything listed inside of github pages docs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import githubPages from 'vite-plugin-github-pages'
 
 export default defineConfig({
   plugins: [react()],
   base: '/mnkrueger/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
